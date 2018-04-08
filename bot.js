bdcaecdd3ea92f95cee75a8e0a3ca7f28f09005a
Zachary Land <dland82995@protonmail.com>
2018-04-08 11:24:56 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+clinet.on('ready', () => {
+  console.log('I am ready!');
+});
+
+clinet.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
+}):
+
+clinet.login(process.env.BOT_TOKEN);
