ef12608a40652db41b1dd9a813c9b17654ce1bb3
Zachary Land <dland82995@protonmail.com>
2018-04-08 16:03:16 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', async message => {
     
     //if(message.content.indexOf(config.prefix) !== 0) return;
     
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    /*const args = message.content.slice(prefix.length).trim().split(/ +/g);
     
     for (var i = 0; i < args.length; i++) {
         
@@ -27,15 +27,16 @@ client.on('message', async message => {
             message.channel.play({files: ["./assets/480.mp4]});
         }
         
-    }
+    }*/
     
-   /* if (message.content.includes('fortnite')) {
+    if (message.content.includes('fortnite')) {
         message.channel.send({files: ["./assets/fortnite_sucks.jpg"]});
     }
     
     if (message.content.includes('aram') || message.content.includes('arams') || message.content.includes('league')) {
         message.channel.send("League of Tanks, Game Never Changes!", {files: ["./assets/league_of_tanks.png"]});
-    }*/
+        message.channel.play({files: ["./assets/480.mp4]});
+    }
     
     /*
     if (message.author.username === 'HazyArc14') {
