0f6d83b4806e028ab12784eb737bb83a5865f1e6
Zachary Land <dland82995@protonmail.com>
2018-04-08 17:40:04 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const talkedRecently = new Set();
+const leagueTalkedRecently = new Set();
+const fortniteTalkedRecently = new Set();
 
 var isReady = true;
 
 // function to return random number 1-4
 function randomWholeNum() {
-  return Math.floor(Math.random() * 4) + 1;
+    return Math.floor(Math.random() * 4) + 1;
 }
 
 client.on('ready', () => {
@@ -18,16 +19,26 @@ client.on('message', async message => {
     
     if(message.author.bot) return;
     
-    if (talkedRecently.has(message.author.id)) {
-            message.channel.send("Wait 1 minute before getting typing this again. - " + message.author);
-    } else {
-    
-        if (message.content.includes('fortnite')) {
+    if (message.content.includes('fortnite')) {
+
+        if (fortniteTalkedRecently.has(message.author.id)) {
+            return;
+        } else {
             message.channel.send({files: ["./assets/fortnite_sucks.jpg"]});
         }
 
-        if (isReady && (message.content.includes('aram') || message.content.includes('arams') || message.content.includes('league'))) {
-            
+        fortniteTalkedRecently.add(message.author.id);
+        setTimeout(() => {
+            fortniteTalkedRecently.delete(message.author.id);
+        }, 60000);
+
+    }
+
+    if (isReady && (message.content.includes('aram') || message.content.includes('arams') || message.content.includes('league'))) {
+
+        if (leagueTalkedRecently.has(message.author.id)) {
+            return;
+        } else {
 
             isReady = false;
             var voiceChannel = message.member.voiceChannel;
@@ -38,46 +49,47 @@ client.on('message', async message => {
                     voiceChannel.join().then(connection => {
                         const dispatcher = connection.playFile("./assets/audio/sameGame.mp3");
                         dispatcher.on("end", end => {
-                          voiceChannel.leave();
+                            voiceChannel.leave();
                         });
                     }).catch(err => console.log(err));
                     break;
-                
+
                 case 2:
                     voiceChannel.join().then(connection => {
                         const dispatcher = connection.playFile("./assets/audio/magicResist.mp3");
                         dispatcher.on("end", end => {
-                          voiceChannel.leave();
+                            voiceChannel.leave();
                         });
                     }).catch(err => console.log(err));
                     break;
-                
+
                 case 3:                
                     voiceChannel.join().then(connection => {
                         const dispatcher = connection.playFile("./assets/audio/goodOleArams.mp3");
                         dispatcher.on("end", end => {
-                          voiceChannel.leave();
+                            voiceChannel.leave();
                         });
                     }).catch(err => console.log(err));
                     break;
-                
+
                 case 4:
                     message.channel.send("League of Tanks, Game Never Changes!", {files: ["./assets/league_of_tanks.png"]});
                     break;
-                
+
               default:
                     message.channel.send("League of Tanks, Game Never Changes!", {files: ["./assets/league_of_tanks.png"]});
             };
 
             isReady = true;
+
+            leagueTalkedRecently.add(message.author.id);
+            setTimeout(() => {
+              // Removes the user from the set after a minute
+              leagueTalkedRecently.delete(message.author.id);
+            }, 60000);
+
         }
-        
-        talkedRecently.add(message.author.id);
-        setTimeout(() => {
-          // Removes the user from the set after a minute
-          talkedRecently.delete(message.author.id);
-        }, 60000);
-        
+
     }
 
 });
