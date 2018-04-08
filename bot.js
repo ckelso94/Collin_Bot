f0f7d2d95025906ea1d1ad9fb375568f5237cbd8
Zachary Land <dland82995@protonmail.com>
2018-04-08 12:45:39 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var user;
+var status;
+var overwatch)data
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -13,6 +17,21 @@ client.on('message', async message => {
     
     if(message.content.indexOf('!overwatch ' === 0) {
        message.channel.send('test');
+    
+        var options = {
+            host: "ow-api.com",
+            path: "/v1/stats/pc/us/${user}/profile",
+            method: "GET",
+        };
+    
+        https.get(options, function(response) {
+            status = response.statusCode;
+            
+            response.on('data', function(chunk) {
+                overwatch_data = JSON.parse(chunk);
+                
+                
+    
     }
     
     if(message.author.username === 'temp') {
