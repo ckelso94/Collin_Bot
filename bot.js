3b5446ab76e3bf29fb7ba519f9e8a8c8c4589597
Zachary Land <dland82995@protonmail.com>
2018-04-26 21:13:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,6 +20,7 @@ function randomWholeNum(value) {
 
 client.on('ready', () => {
     console.log('I am ready!');
+    client.user.setGame('Your Mom!')
 });
 
 client.on('message', async message => {
