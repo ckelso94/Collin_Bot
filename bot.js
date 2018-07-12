5e2b7100de9e1d53caf24d2c4cf07efd8816e104
Zachary Land <dland82995@protonmail.com>
2018-07-11 22:36:13 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -215,7 +215,7 @@ client.on('message', async message => {
 
       var helpResponse = "```Since Your Little Bitch Ass Can't Remember Shit!\n\n" +
       "Presense Triggers:\n!setGame Overwatch\n!setListening Spotify\n!setWatching Youtube\n\n" +
-      "Audio Triggers:\n!aram\n!dumbassGame\n!horn\n!kirk\n!lag\n!licker\n!magicResist\n!sameGame\n!snap\n!tinsel\n!yooo\n!warus\n\n" +
+      "Audio Triggers:\n!aram\n!dumbassGame\n!horn\n!kirk\n!lag\n!licker\n!magicResist\n!sameGame\n!snap\n!tinsel\n!yooo\n!warus\n!watch\n\n" +
       "Image Triggers:\n!prime\n!zieg\n\n" +
       "Keywords: (black, fortnite, tank, mexican)```"
       triggerMessage(message, "help", helpResponse, true);
@@ -306,6 +306,11 @@ client.on('message', async message => {
 
     }
     if (message.content.indexOf('!warus') === 0) {
+
+      triggerAudio(message, "warus");
+
+    }
+    if (message.content.indexOf('!watch') === 0) {
 
       triggerAudio(message, "warus");
 
