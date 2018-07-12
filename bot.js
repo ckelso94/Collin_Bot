e77b9d7572d7790e3915fd924a7ac36e7c31ec14
Zachary Land <dland82995@protonmail.com>
2018-07-11 20:51:00 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -353,34 +353,34 @@ client.on('message', async message => {
 
 });
 
-client.on('voiceStateUpdate', (oldMember, newMember) => {
+// client.on('voiceStateUpdate', (oldMember, newMember) => {
   
-    let newUserChannel = newMember.voiceChannel
-    let oldUserChannel = oldMember.voiceChannel
+//     let newUserChannel = newMember.voiceChannel
+//     let oldUserChannel = oldMember.voiceChannel
     
-    let channels = newMember.guild.channels;
-    var generalChannel = (client.guilds.get(newMember.guild.id)).channels.find("name", "general");
+//     let channels = newMember.guild.channels;
+//     var generalChannel = (client.guilds.get(newMember.guild.id)).channels.find("name", "general");
     
-    if (newMember.id === '93105200365043712') {
+//     if (newMember.id === '93105200365043712') {
         
-        if(oldUserChannel === undefined && newUserChannel !== undefined) {
+//         if(oldUserChannel === undefined && newUserChannel !== undefined) {
             
-            randomNum = randomWholeNum(10);
-            console.log('randomNum: ' + randomNum);
+//             randomNum = randomWholeNum(10);
+//             console.log('randomNum: ' + randomNum);
             
-            if (randomNum === 1) {
-                generalChannel.send("Hey, Eeyore (Kelso) is here!", {files: ["./assets/images/eeyore.png"]});
-            }
+//             if (randomNum === 1) {
+//                 generalChannel.send("Hey, Eeyore (Kelso) is here!", {files: ["./assets/images/eeyore.png"]});
+//             }
 
-        } else if(newUserChannel === undefined){
+//         } else if(newUserChannel === undefined){
 
-            // User leaves a voice channel
+//             // User leaves a voice channel
 
-        }
+//         }
         
-    }
+//     }
     
-})
+// })
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
