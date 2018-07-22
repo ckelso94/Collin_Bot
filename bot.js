a82452fce177992dcab2da72ace6c8b56f5aedd0
Zachary Land <dland82995@protonmail.com>
2018-07-22 00:47:09 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -220,7 +220,7 @@ client.on('message', async message => {
 
       var helpResponse = "```Since Your Little Bitch Ass Can't Remember Shit!\n\n" +
       "Presense Triggers:\n!setGame Overwatch\n!setListening Spotify\n!setWatching Youtube\n\n" +
-      "Audio Triggers:\n!aram\n!comeOn\n!dumbassGame\n!horn\n!kirk\n!lag\n!licker\n!magicResist\n!monkey\n!sameGame\n!snap\n!tinsel\n!yooo\n!warus\n!watch\n\n" +
+      "Audio Triggers:\n!aram\n!celsoHere\n!comeOn\n!dumbassGame\n!horn\n!kirk\n!lag\n!licker\n!magicResist\n!monkey\n!sameGame\n!snap\n!tinsel\n!yooo\n!warus\n!watch\n\n" +
       "Image Triggers:\n!prime\n!zieg\n\n" +
       "Keywords: (black, fortnite, tank, mexican)```"
       triggerMessage(message, "help", helpResponse, true);
@@ -263,6 +263,15 @@ client.on('message', async message => {
         triggerAudio(message, "goodOleArams", "");
       } else {
         triggerAudio(message, "goodOleArams", splitMessage[1]);
+      }
+
+    }
+    if (message.content.indexOf('!celsoHere') === 0) {
+
+      if (typeof splitMessage[1] === 'undefined') {
+        triggerAudio(message, "celsoHere", "");
+      } else {
+        triggerAudio(message, "celsoHere", splitMessage[1]);
       }
 
     }
