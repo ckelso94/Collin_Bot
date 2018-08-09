1ed962c4dde1c0bdcdc3bab2d6bf3e3b9338edc5
Zachary Land <dland82995@protonmail.com>
2018-08-08 20:09:13 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -220,7 +220,7 @@ client.on('message', async message => {
 
       var helpResponse = "```Since Your Little Bitch Ass Can't Remember Shit!\n\n" +
       "Presense Triggers:\n!setGame Overwatch\n!setListening Spotify\n!setWatching Youtube\n\n" +
-      "Audio Triggers:\n!aram\n!celsoHere\n!croissant\n!dumbassGame\n!fortFucker\n!horn\n!kirk\n!lag\n!licker\n!magicResist\n!monkey\n!sameGame\n!snap\n!tinsel\n!yooo\n!warus\n!watch\n\n" +
+      "Audio Triggers:\n!aram\n!celsoHere\n!croissant\n!dumbassGame\n!fortFucker\n!horn\n!kirk\n!lag\n!licker\n!magicResist\n!monkey\n!neck\n!sameGame\n!snap\n!tinsel\n!yooo\n!warus\n!watch\n\n" +
       "Image Triggers:\n!prime\n!zieg\n\n" +
       "Keywords: (black, fortnite, tank, mexican)```"
       triggerMessage(message, "help", helpResponse, true);
@@ -380,6 +380,15 @@ client.on('message', async message => {
         triggerAudio(message, "monkey", "");
       } else {
         triggerAudio(message, "monkey", splitMessage[1]);
+      }
+
+    }
+    if (message.content.indexOf('!neck') === 0) {
+
+      if (typeof splitMessage[1] === 'undefined') {
+        triggerAudio(message, "neck", "");
+      } else {
+        triggerAudio(message, "neck", splitMessage[1]);
       }
 
     }
